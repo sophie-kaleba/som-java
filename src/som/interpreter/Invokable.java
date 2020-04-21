4708bafc3d3a74170303ebe4474ebe2f7c01c81b
Sophie Kaleba <sophie.kaleba@gmail.com>
2020-04-21 11:08:53 +0100

    add a wrapper for SMethod, makes it inheriting from RootNode

---
+++ src/som/interpreter/Invokable.java
@@ -0,0 +1,12 @@
+package som.interpreter;
+
+import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.nodes.RootNode;
+
+public abstract class Invokable extends RootNode {
+
+    protected Invokable(TruffleLanguage<?> language) {
+        super(language);
+    }
+
+}
