0f345fc3c17f64dad2b65fe2cb589d3f08aec115
Sophie Kaleba <sophie.kaleba@gmail.com>
2020-07-20 15:04:00 +0100

    add serial number to address compilation warning

--- src/som/interpreter/RestartLoopException.java
+++ src/som/interpreter/RestartLoopException.java
@@ -2,6 +2,8 @@ package som.interpreter;
 
 import com.oracle.truffle.api.nodes.ControlFlowException;
 
+
 public class RestartLoopException extends ControlFlowException {
 
+  private static final long serialVersionUID = -509673786811277307L;
 }
