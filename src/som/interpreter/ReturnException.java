dea7d9248b15e6f285ff850fc6383f49fcb979f5
Sophie Kaleba <sophie.kaleba@gmail.com>
2020-08-05 23:59:04 +0100

    fix style on return exception

--- src/som/interpreter/ReturnException.java
+++ src/som/interpreter/ReturnException.java
@@ -1,27 +1,30 @@
 package som.interpreter;
 
 import com.oracle.truffle.api.nodes.ControlFlowException;
+
 import som.vmobjects.SAbstractObject;
+
 
 public class ReturnException extends ControlFlowException {
 
-    private static final long serialVersionUID = 6317360573982456256L;
-    private final SAbstractObject result;
-    private final Frame outerContext;
+  private static final long     serialVersionUID = 6317360573982456256L;
+  private final SAbstractObject result;
+  private final Frame           outerContext;
 
-    public ReturnException(SAbstractObject result, Frame target) {
-        this.result = result;
-        this.outerContext = target;
-    }
+  public ReturnException(SAbstractObject result, Frame target) {
+    this.result = result;
+    this.outerContext = target;
+  }
 
-    public SAbstractObject getResult() {
-        return result;
-    }
+  public SAbstractObject getResult() {
+    return result;
+  }
 
-    public Frame getTarget() { return this.outerContext; }
+  public Frame getTarget() {
+    return this.outerContext;
+  }
 
-    public boolean hasReachedTarget(Frame current) {
-        return current == outerContext;
-    }
+  public boolean hasReachedTarget(Frame current) {
+    return current == outerContext;
+  }
 }
-
